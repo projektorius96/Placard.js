b9718c4de87d12fe11388aedb21d151ff9377384
Lukas Gaucas <34185919+projektorius96@users.noreply.github.com>
2024-12-20 20:29:27 +0200

    this is pseudo-amending commit for commit:48eae87

--- implementation/index.js
+++ implementation/index.js
@@ -2,8 +2,60 @@ import Ring from './ring';
 import Wireframe from './wireframe';
 import RightTriangle from './right-triangle';
 
-export {
-    Ring,
-    Wireframe,
-    RightTriangle,
+import Placard from '../src'
+import UserSettings from '../user-settings';
+
+export default function (stage){
+
+    const { setAngle } = Placard.Helpers.Trigonometry;
+
+    Placard
+    .init({stage, stageScale: 25 /* <=== # thumb of rule is between 15-20 (in relative units) */})
+    .on((context)=>{
+
+        if ( UserSettings.init({context}) ) {
+
+            let canvas = context.canvas;
+            
+            switch (canvas.name) {
+
+                /* === GRID === */
+                case 'grid' :
+                    Placard.Views.Grid.draw({
+                        canvas: stage.layers.grid, 
+                        options: {
+                            lineWidth: 2,
+                        }}
+                    );
+                break;
+
+                /* === RIGHT-TRIANGLE === */
+                case stage.layers.right_triangle.name :
+                    // DEV_NOTE # The line below control grouped (i.e. Layer-level) matrix transformation:
+                    context.setTransform(...setAngle(0), stage.grid.X_IN_MIDDLE, stage.grid.Y_IN_MIDDLE);
+
+                    RightTriangle.draw({context});
+                break;
+                
+                /* === WIREFRAMES === */
+                case stage.layers.wireframe.name :
+                    Wireframe.draw({context});
+                break;
+
+                /* === RING === */
+                case stage.layers.ring.name :
+                    // DEV_NOTE # The line below control grouped (i.e. Layer-level) matrix transformation:
+                    context.setTransform(...setAngle(0), stage.grid.X_IN_MIDDLE, stage.grid.Y_IN_MIDDLE);
+                    
+                    Ring.draw({context});
+                break ;
+
+            endswitch:;}
+
+        endif:;}
+        
+    endon:;})
+
+    return true;
+
 }
